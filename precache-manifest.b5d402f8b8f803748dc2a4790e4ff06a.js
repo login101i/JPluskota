34e4c2a1b48d19c75b606af49bebaa50c05897ca
Maciej Kruszyniak <maciejkruszyniak@gmail.com>
2020-08-14 23:18:51 +0200

    Updates

---
+++ precache-manifest.b5d402f8b8f803748dc2a4790e4ff06a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb34d35a9bb06ea09c0c356e481a6b5f",
+    "url": "/JPluskota/index.html"
+  },
+  {
+    "revision": "00503691ad4746dc5eef",
+    "url": "/JPluskota/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "52ea8ea9c2eb7f0087ba",
+    "url": "/JPluskota/static/css/main.c707ed00.chunk.css"
+  },
+  {
+    "revision": "00503691ad4746dc5eef",
+    "url": "/JPluskota/static/js/2.35ab5b5d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/JPluskota/static/js/2.35ab5b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52ea8ea9c2eb7f0087ba",
+    "url": "/JPluskota/static/js/main.b2ba01e9.chunk.js"
+  },
+  {
+    "revision": "a9f44b03a8471910ad98",
+    "url": "/JPluskota/static/js/runtime-main.4acb5afe.js"
+  },
+  {
+    "revision": "ca64d155f0b2bff91d6039ccd1c7b979",
+    "url": "/JPluskota/static/media/defaultImg.ca64d155.jpg"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/JPluskota/static/media/logo.9f7e506d.svg"
+  }
+]);
